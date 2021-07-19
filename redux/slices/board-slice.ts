f6f582e1c177a987cd20630a1ea822cb6b1e634f
Konstantin-Koktashev <lexxtalionis33@gmail.com>
2021-07-20 00:10:57 +0300

    almost done

--- redux/slices/board-slice.ts
+++ redux/slices/board-slice.ts
@@ -1,16 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { RootState } from "../store";
+
 const initialState = {
   value: 0,
   status: "idle",
   counter: 0,
 };
-
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
 
 export const boardSlice = createSlice({
   name: "board",
@@ -18,16 +13,11 @@ export const boardSlice = createSlice({
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
     increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
       state.value += 1;
     },
     decrement: (state) => {
       state.value -= 1;
     },
-    // Use the PayloadAction type to declare the contents of `action.payload`
     incrementByAmount: (state, action) => {
       state.value += action.payload;
     },
@@ -39,12 +29,6 @@ export const boardSlice = createSlice({
 
 export const { increment, decrement, incrementByAmount } = boardSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectCount = (state: RootState) => state.board.counter;
-
-// We can also write thunks by hand, which may contain both sync and async logic.
-// Here's an example of conditionally dispatching actions based on current state.
 
 export default boardSlice.reducer;
